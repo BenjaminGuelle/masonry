2cfee27006ca605c9fb335eb079819883bf928dc
Benjamin Guelle <benjamin.guelle@gmail.com>
2021-02-12 13:35:41 +0100

    update navigation on mobile & start js card

--- js/index.js
+++ js/index.js
@@ -16,3 +16,41 @@ function handletogglerMenu() {
         line.classList.add('active');
     }
 };
+
+const moreInfo = document.querySelectorAll('.btn-card');
+console.log(moreInfo);
+
+for ( let i = 0; i < moreInfo.length; i++) {
+    moreInfo[i].addEventListener('click', (evt) => {
+        handleDisplayService(evt), false;
+    });
+}
+
+function handleDisplayService(evt) {
+    evt.preventDefault();
+    const currentCard = evt.currentTarget.parentNode;
+    console.log('CLICK', currentCard);
+
+    const card1 = document.querySelector('.card1');
+    const card2 = document.querySelector('.card2');
+    const card3 = document.querySelector('.card3');
+
+    if ( currentCard.classList.contains('card1') ) {
+        card1.classList.add('active');
+        card2.classList.add('notActive');
+        card3.classList.add('notActive');
+    }
+
+    else if ( currentCard.classList.contains('card2') ) {
+        card2.classList.add('active');
+        card1.classList.add('notActive');
+        card3.classList.add('notActive');
+    }
+    
+    else if ( currentCard.classList.contains('card3') ) {
+        card3.classList.add('active');
+        card2.classList.add('notActive');
+        card1.classList.add('notActive');
+    }
+
+}
